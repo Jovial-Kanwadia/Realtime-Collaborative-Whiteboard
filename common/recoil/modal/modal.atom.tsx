bd59df22d2036ebc24a60b919730e3906b752795
aryankad1an <aryankaddian842@gmail.com>
2025-04-16 15:10:39 +0530

    Background component added

---
+++ common/recoil/modal/modal.atom.tsx
@@ -0,0 +1,13 @@
+import { JSX } from "react";
+import { atom } from "recoil";
+
+export const modalAtom = atom<{
+  modal: JSX.Element | JSX.Element[];
+  opened: boolean;
+}>({
+  key: "modal",
+  default: {
+    modal: <></>,
+    opened: false,
+  },
+});
